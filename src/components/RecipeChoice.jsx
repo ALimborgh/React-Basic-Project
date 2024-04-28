f8927284d96767ecea564119873c38e12a1e5dfa
Adriaen Limborgh <adriaen95@hotmail.com>
2024-04-28 18:13:52 +0200

    Displayed everyting exept lifestyle choice

--- src/components/RecipeChoice.jsx
+++ src/components/RecipeChoice.jsx
@@ -1,25 +1,68 @@
 import { recipes } from "../utils/data.js";
-import { Card, Stack, CardBody, Heading, Text, Image, Highlight } from '@chakra-ui/react'
-
+import { Box, Card, Stack, CardBody, Heading, Text, SimpleGrid, Highlight} from '@chakra-ui/react'
 
 export const RecipeChoice = () => {
+    const handleCardClick = (recipe) => {
+        console.log(recipe);
+    };
+
     return (
-        <>
-            {recipes.map((recipe) => (
-                <Card maxW='sm' key={recipe.id}>
-                    <CardBody>
-                        <Image
-                            src={recipe.image}
-                            borderRadius='lg'
-                        />
-                        <Stack mt='6' spacing='3'>
-                            <Text as='sub'>{recipe.mealType}</Text>
-                            <Heading size='md'>{recipe.label}</Heading>
-                            <Highlight> {recipe.cautions} </Highlight>
-                        </Stack>
-                    </CardBody>
-                </Card>
-            ))}
-        </>
+        <Box>
+            <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+                {recipes.map((recipe) => (
+                    <Card 
+                        key={recipe.id} 
+                        onClick={() => handleCardClick(recipe)} 
+                        bg="white.500"
+                        color="white"
+                        boxShadow="lg"
+                        _hover={{ bg: "blue.100" }}
+                    >
+                        <CardBody>
+                            <Box 
+                                as="img"
+                                src={recipe.image} 
+                                objectFit='cover'
+                                objectPosition="center" 
+                                width="100%" 
+                                height="150px"
+                            />
+                            <Stack mt='6' spacing='3' textColor='black'>
+                                <Text as='sub' fontSize="sm">{recipe.mealType}</Text>
+                                <Heading size='sm'>{recipe.label}</Heading>
+                                {recipe.dietLabels.length > 0 &&
+                                <Text fontSize='xs'>
+                                    Diet:{""}
+                                    {recipe.dietLabels.map((diet) => (
+                                        <Highlight key={recipe.id} query={diet} styles={{ px: '1', py: '1', bg: 'green.300' }}>
+                                            {diet + ' '}
+                                        </Highlight>
+                                    ))}
+                                </Text>
+                                }
+                                <Text fontSize='xs'>
+                                    Dish:{""}
+                                    {recipe.dishType.map((dish) => (
+                                        <Highlight key={recipe.id} query={dish} styles={{ px: '1', py: '1', as:'em'}}>
+                                            {dish}
+                                        </Highlight>
+                                    ))}
+                                </Text>
+                                {recipe.cautions.length > 0 && 
+                                <Text fontSize="xs">
+                                    Cautions:{""}
+                                    {recipe.cautions.map((caution) => (
+                                            <Highlight key={recipe.id} query={caution} styles={{ px: '1', py: '1', bg: 'red.300' }}>
+                                                {caution + ' '}
+                                            </Highlight>
+                                    ))}
+                                </Text>
+                                }
+                            </Stack>
+                        </CardBody>
+                    </Card>
+                ))}
+            </SimpleGrid>
+        </Box>
     );
-};
+}; 
