c73f202ae3cf8dc4f0b75d2f4e3c30b685da6be5
Adriaen Limborgh <adriaen95@hotmail.com>
2024-04-29 12:22:31 +0200

    Refactor RecipeChoice component to improve card layout and styling

--- src/components/RecipeChoice.jsx
+++ src/components/RecipeChoice.jsx
@@ -12,56 +12,59 @@ export const RecipeChoice = ({recipes, clickfn}) => {
         <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
                 {recipes.map((recipe, index) => (
                     <Link to={`/recipe/${encodeURIComponent(recipe.label)}`} key={index} onClick={() => handleCardClick(recipe.name)}>
-                        <Card 
-                            bg="white.500"
-                            color="white"
-                            boxShadow="lg"
-                            _hover={{ bg: "blue.100" }}
-                            border = "1px solid"
-                        >
-                            <CardBody>
-                                <Box 
-                                    as="img"
-                                    src={recipe.image} 
-                                    objectFit='cover'
-                                    objectPosition="center" 
-                                    width="100%" 
-                                    height="150px"
-                                />
-                                <Stack mt='6' spacing='3' textColor='black'>
-                                    <Text as='sub' fontSize="sm">{recipe.mealType}</Text>
-                                    <Heading size='sm'>{recipe.label}</Heading>
-                                    {recipe.dietLabels.length > 0 && 
-                                    <Text fontSize='xs'>
-                                        Diet:{""}
-                                        {recipe.dietLabels.map((diet, index) => (
-                                            <Highlight key={`${recipe.id}-diet-${index}`} query={diet} styles={{ px: '2px', py: 'px', bg: 'green.300', borderRadius: 'md' }}>
-                                                {diet + ' '}
-                                            </Highlight>
-                                        ))}
-                                    </Text>
-                                    }
-                                    <Text fontSize='xs'>
-                                        Dish:{""}
-                                        {recipe.dishType.map((dish, index) => (
-                                            <Highlight key={`${recipe.id}-dish-${index}`} query={dish} styles={{ px: '2px', py: '1px', as:'em', borderRadius: 'md'}}>
-                                                {dish}
-                                            </Highlight>
-                                        ))}
-                                    </Text>
-                                    {recipe.cautions.length > 0 && 
-                                    <Text fontSize="xs">
-                                        Cautions:{""}
-                                        {recipe.cautions.map((caution, index) => (
-                                                <Highlight key={`${recipe.id}-caution-${index}`} query={caution} styles={{ px: '2px', py: '1px', bg: 'red.300', borderRadius: 'md' }}>
-                                                    {caution + ' '}
+                        <Box display="flex" flexDirection="column" height="100%">
+                            <Card 
+                                bg="white.500"
+                                color="white"
+                                boxShadow="lg"
+                                _hover={{ bg: "blue.100" }}
+                                border = "1px solid"
+                                minHeight="350px"
+                            >
+                                <CardBody flex="1 0 auto">
+                                    <Box 
+                                        as="img"
+                                        src={recipe.image} 
+                                        objectFit='cover'
+                                        objectPosition="center" 
+                                        width="100%" 
+                                        height="150px"
+                                    />
+                                    <Stack mt='6' spacing='3' textColor='black'>
+                                        <Text as='sub' fontSize="sm">{recipe.mealType}</Text>
+                                        <Heading size='sm'>{recipe.label}</Heading>
+                                        {recipe.dietLabels.length > 0 && 
+                                        <Text fontSize='xs'>
+                                            Diet:{""}
+                                            {recipe.dietLabels.map((diet, index) => (
+                                                <Highlight key={`${recipe.id}-diet-${index}`} query={diet} styles={{ px: '2px', py: 'px', bg: 'green.300', borderRadius: 'md' }}>
+                                                    {diet + ' '}
                                                 </Highlight>
-                                        ))}
-                                    </Text>
-                                    }
-                                </Stack>
-                            </CardBody>
-                        </Card>
+                                            ))}
+                                        </Text>
+                                        }
+                                        <Text fontSize='xs'>
+                                            Dish:{""}
+                                            {recipe.dishType.map((dish, index) => (
+                                                <Highlight key={`${recipe.id}-dish-${index}`} query={dish} styles={{ px: '2px', py: '1px', as:'em', borderRadius: 'md'}}>
+                                                    {dish}
+                                                </Highlight>
+                                            ))}
+                                        </Text>
+                                        {recipe.cautions.length > 0 && 
+                                        <Text fontSize="xs">
+                                            Cautions:{""}
+                                            {recipe.cautions.map((caution, index) => (
+                                                    <Highlight key={`${recipe.id}-caution-${index}`} query={caution} styles={{ px: '2px', py: '1px', bg: 'red.300', borderRadius: 'md' }}>
+                                                        {caution + ' '}
+                                                    </Highlight>
+                                            ))}
+                                        </Text>
+                                        }
+                                    </Stack>
+                                </CardBody>
+                            </Card>
+                        </Box>
                     </Link>
                 ))}
             </SimpleGrid>
