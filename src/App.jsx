edf7e4cd4374cb0b0153725defb928e72551cfe5
Adriaen Limborgh <adriaen95@hotmail.com>
2024-04-29 10:01:19 +0200

    Fix background color and dimensions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { RecipeListPage } from "./pages/RecipeListPage";
 
 export const App = () => {
    return (
-     <Box bg='teal.600' w='100vw' h='100vh'>
+     <Box bg='teal.600' w='99vw' h='250vh'>
         <RecipeListPage />
      </Box>
     );
