b3ae29f294a8dab5d771f1f511b65cedc8424267
Adriaen Limborgh <adriaen95@hotmail.com>
2024-04-29 10:11:22 +0200

    Refactor App component to fix background color and dimensions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { RecipeListPage } from "./pages/RecipeListPage";
 
 export const App = () => {
    return (
-     <Box bg='teal.600' w='99vw' h='250vh'>
+     <Box bg='teal.600' w='auto' h='auto' p={4}>
         <RecipeListPage />
      </Box>
     );
