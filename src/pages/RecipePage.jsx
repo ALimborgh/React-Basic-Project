6c43e2f13b24dea2cfbae8abaead14d89bb7de51
Adriaen Limborgh <adriaen95@hotmail.com>
2024-04-29 13:34:35 +0200

    Refactor RecipePage component to use Chakra UI for styling and add navigation functionality

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,10 +1,13 @@
 import {recipes} from '../utils/data';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Card, CardBody, SimpleGrid, Box, Stack, Text, Highlight, Grid } from '@chakra-ui/react';
+import { Button } from '../components/ui/ButtonInput';
 
 export const RecipePage = () => {
     const { label } = useParams();
     const decodedLabel = decodeURIComponent(label);
     const recipeData = recipes.find(recipe => recipe.label === decodedLabel);
+    const navigate = useNavigate();
     
     if (!recipeData) {
         return <h1>Recipe not found!</h1>;
@@ -17,23 +20,85 @@ export const RecipePage = () => {
         dietLabels: recipeData.dietLabels,
         dishType: recipeData.dishType,
         cautions: recipeData.cautions,
-        servings: recipeData.servings,
-        calories: recipeData.calories,
+        servings: recipeData.yield,
+        totalNutrients: recipeData.totalNutrients,
         totalTime: recipeData.totalTime,
         healthLabels: recipeData.healthLabels,
 
     };
 
     return (
-        <div>
-            <h1>{recipe.title}</h1>
-            <img src={recipe.imageUrl} alt={recipe.title} />
-            <h2>Ingredients:</h2>
-            <ul>
-                {recipe.ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
-                ))}
-            </ul>
-        </div>
+        <Card 
+            bg="white.500"
+            color="black"
+            boxShadow="lg"
+            border = "1px solid"
+        >
+            <CardBody p={4}>
+                <Button color="Black" _hover={{ bg: "teal.100" }} bg="teal.400" onClick={() =>navigate(-1)} mb={1} > Previous </Button>
+                <Box 
+                    as="img"
+                    src={recipe.imageUrl}
+                    objectFit='cover'
+                    objectPosition="center" 
+                    width="100%" 
+                    height="600px"
+                    borderRadius='md'
+                />
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <Stack>
+                        <Text>{recipe.mealType}</Text>
+                        <Text>{recipe.title}</Text>
+                        <Text>Time: {recipe.totalTime} min</Text>
+                        <Text>Servings: {recipe.servings}</Text>
+                        <Text>Ingredients: </Text>
+                        {recipe.ingredients.map((ingredient, index) => (
+                            <Text key={index}>- {ingredient}</Text>
+                        ))}
+                    </Stack>
+                    <Stack>
+                        <Text>Health Labels:</Text>
+                        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={2}>
+                            {recipe.healthLabels.map((label, index) => (
+                                <Box key={index} bg="yellow.200" p={1}>
+                                    <Text>- {String(label)}</Text>
+                                </Box>
+                            ))}
+                        </SimpleGrid>
+                        <Text>Diet Labels:</Text>
+                        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={2}>
+                            {recipe.dietLabels.map((label, index) => (
+                                <Box key={index} bg="yellow.200" p={1}>
+                                    <Text>- {String(label)}</Text>
+                                </Box>
+                            ))}
+                        </SimpleGrid>
+                        <Text>Cautions:</Text>
+                        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={2}>
+                            {recipe.cautions.map((caution, index) => (
+                                <Box key={index} bg="yellow.200" p={1}>
+                                    <Text>- {caution}</Text>
+                                </Box>
+                            ))}
+                        </SimpleGrid>
+                        <Text>Nutrients:</Text>
+                        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={2}>
+                            {Object.entries(recipeData.totalNutrients).map(([nutrient, data], index) => {
+                                if (['ENERC_KCAL', 'PROCNT', 'FAT', 'CHOCDF', 'CHOLE', 'NA'].includes(nutrient)) {
+                                    return (
+                                       <Box key={index} bg="yellow.200" p={1}> 
+                                            <Text>
+                                                - {data.label}: {Math.ceil(data.quantity)} {data.unit}
+                                            </Text>
+                                        </Box>
+                                    );
+                                }
+                                return null;
+                            })}
+                        </SimpleGrid>
+                    </Stack>
+                </Grid>
+            </CardBody>
+        </Card>
     );
 };
